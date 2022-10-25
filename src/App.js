8a06a70d2f5b41de9c703f3c81f89470317936ae
Jiali Zhang <zhangjiali86@gmail.com>
2022-10-25 20:23:28 +0200

    finishe without CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
   const [newTask, setNewTask] = useState("")
@@ -9,30 +9,34 @@ function App() {
     setNewTask(event.target.value)
   }
 
-  const  addNewEvent = () => {
-  //   if (!newTask.text || /^\s*$/.test(newTask.text)) {
-  //     return
-  // }
-
-    const task={
+  const addNewEvent = (event) => {
+    const task = {
       id: Math.floor(Math.random() * 10000),
       taskName: newTask
     }
-    setTodoList([...todoList, task])
+    if (!newTask || /^\s*$/.test(newTask)) {
+      return
+    }
+    setTodoList([task, ...todoList])
     setNewTask("")
   }
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="App">
-    <input onChange={handelChange}  value={newTask}/>
-    <button onClick={addNewEvent}>Add a event</button>
-    {todoList.map((task, key)=> {
-      return (
-        <div key={task.id}>
-          {task.taskName}
-          <button>x</button>
-        </div>
-      )
-    })}
+      <input onChange={handelChange} value={newTask} />
+      <button onClick={addNewEvent}>Add a event</button>
+      {todoList.map((task, key) => {
+        return (
+          <div key={task.id}>
+            {task.taskName}
+            <button onClick={() => deleteTask(task.id)}>x</button>
+          </div>
+        )
+      })}
     </div>
   );
 }
